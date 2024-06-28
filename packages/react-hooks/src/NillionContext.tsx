2254f3e23b414c6a8f19b9e0a6b8b5e49f62493f
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-06-28 17:38:08 +0100

    feat(hooks): add useNillionBootstrap hook

--- packages/react-hooks/src/NillionContext.tsx
+++ packages/react-hooks/src/NillionContext.tsx
@@ -1,9 +1,53 @@
 import { NillionClient, NillionConfig } from "@nillion/core";
-import React, { ReactNode, createContext, useContext } from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type Bootstrap =
+  | {
+      client: undefined;
+      error: string;
+      ready: false;
+    }
+  | {
+      client: NillionClient;
+      error: undefined;
+      ready: true;
+    };
+
+export function useNillionBootstrap(config: NillionConfig): Bootstrap {
+  const [_ready, setReady] = useState(false);
+  const [error, setError] = useState("");
+  const [client, setClient] = useState<NillionClient>();
+
+  useEffect(() => {
+    async function bootstrap() {
+      await NillionClient.init();
+      const client = NillionClient.fromConfig(config);
+      setReady(true);
+      setClient(client);
+    }
+
+    bootstrap().catch((e) => {
+      console.error(e);
+      setError("failed to initialise nillion client");
+    });
+  }, []);
+
+  if (client) {
+    return { client, error: undefined, ready: true };
+  } else {
+    return { client: undefined, error, ready: false };
+  }
+}
 
 export type Context = {
-  config: NillionConfig;
   client: NillionClient;
+  config: NillionConfig;
 };
 
 export const NillionContext = createContext<Context | undefined>(undefined);
@@ -17,19 +61,22 @@ export function useNillion() {
 }
 
 export type NillionProviderValue = {
+  client: NillionClient;
   config: NillionConfig;
 };
 
 export type NillionProviderProps = {
   children: ReactNode;
-  value: Context;
+  value: NillionProviderValue;
 };
 
 export function NillionProvider(
   props: NillionProviderProps,
 ): React.ReactElement {
   return (
-    <NillionContext.Provider value={props.value}>
+    <NillionContext.Provider
+      value={{ client: props.value.client, config: props.value.config }}
+    >
       {props.children}
     </NillionContext.Provider>
   );
--- packages/react-hooks/src/index.test.ts
+++ packages/react-hooks/src/index.test.ts
@@ -1,5 +1,3 @@
-import { add } from "./index";
-
 test("react-hooks", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(3).toBe(3);
 });
