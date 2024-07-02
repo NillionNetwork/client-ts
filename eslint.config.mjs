db76f3023a930f2efe1ba75b1530f730c2e40032
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-07-02 10:08:55 +0100

    style: tweak eslint rules and tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,14 +13,12 @@ export default [
   {
     rules: {
       "sort-keys": "warn",
-      "sort-imports": "warn",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
           args: "all",
           argsIgnorePattern: "^_",
           caughtErrors: "all",
-          caughtErrorsIgnorePattern: "^_",
           destructuredArrayIgnorePattern: "^_",
           varsIgnorePattern: "^_",
           ignoreRestSiblings: true,
