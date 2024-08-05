9ee3e4670eeb6ad0e13bd4979ec3b88fb23f10d0
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-08-05 08:19:03 -0400

    fix(wasm): mimic debug behaviour with custom log function

--- packages/client-wasm/dist/start-worker.js
+++ packages/client-wasm/dist/start-worker.js
@@ -1,5 +1,7 @@
+import { Log } from "./logging.js";
+
 export function startWorker(module, memory, state, opts, helper) {
-  console.log("nillion:wasm Loading wasm web worker.");
+  Log("Loading wasm web worker.");
   const worker = new Worker(new URL("./worker.js", import.meta.url), opts);
   worker.postMessage([module, memory, state, helper.mainJS()]);
   return Promise.resolve();
