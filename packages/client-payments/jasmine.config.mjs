33c85554df3d9cb3120f00c52c99a30578993716
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-08-09 13:36:26 -0400

    feat: use common base config for webpack, jasmine, esbuild

--- packages/client-payments/jasmine.config.mjs
+++ packages/client-payments/jasmine.config.mjs
@@ -1,40 +1,11 @@
 import path from "node:path";
 import proxy from "express-http-proxy";
-import configFixture from "../fixture/network.json" with { type: "json" };
 import { fileURLToPath } from "node:url";
+import { buildJasmineBaseConfig } from "../resources/jasmine.base.config.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log(__dirname);
+const packageBaseDir = path.dirname(__filename);
 
 export default {
-  port: 9191,
-  srcDir: "../fixture",
-  srcFiles: [],
-  specDir: "dist/test",
-  specFiles: ["test.js"],
-  helpers: ["helpers"],
-  env: {
-    stopSpecOnExpectationFailure: false,
-    stopOnSpecFailure: false,
-    random: false,
-  },
-  browser: {
-    name: "headlessChrome",
-  },
-  middleware: {
-    "/": (req, res, next) => {
-      res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Cross-Origin-Embedder-Policy": "require-corp",
-        "Cross-Origin-Opener-Policy": "same-origin",
-      });
-      next();
-    },
-    "/(:filename).(wasm|js)": (req, res) => {
-      res.sendFile(path.resolve(__dirname, `dist/${req.baseUrl}`));
-    },
-    "/nilchain": proxy(configFixture.payments_rpc_endpoint),
-  },
+  ...buildJasmineBaseConfig(packageBaseDir),
 };
---
+++ packages/resources/webpack.base.config.mjs
@@ -0,0 +1,52 @@
+import path from "node:path";
+import * as glob from "glob";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import webpack from "webpack";
+
+export const buildWebpackBaseConfig = (packageBaseDir) => ({
+  mode: "development",
+  entry: glob.sync("tests/**/*.test.ts"),
+  resolve: {
+    modules: [packageBaseDir, "tests", "test-helpers", "node_modules"],
+    extensions: [".js", ".ts", ".json"],
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+    fallback: {
+      crypto: false,
+      buffer: false,
+      stream: false,
+      vm: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.wasm$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      "NILLION_BOOTNODE_WEBSOCKET",
+      "NILLION_CLUSTER_ID",
+      "NILLION_NILCHAIN_CHAIN_ID",
+      "NILLION_NILCHAIN_JSON_RPC",
+      "NILLION_NILCHAIN_PRIVATE_KEY_0",
+      "NILLION_NODE_SEED",
+      "NILLION_USER_SEED",
+      "NILLION_TEST_PROGRAMS_NAMESPACE",
+    ]),
+  ],
+  devtool: "inline-source-map",
+  output: {
+    filename: (pathData) =>
+      pathData.chunk.name === "main" ? "test/test.js" : "src/[name].js",
+    path: path.resolve(packageBaseDir, "dist-test"),
+    clean: true,
+  },
+});
