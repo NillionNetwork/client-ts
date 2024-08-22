453f48e631572d3ec8ff0cfcfd6cac3bf4fb457f
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-08-22 12:02:56 +0100

    feat(core): use lavender5 for Photon nilchain endpoint

--- packages/client-core/src/configs.ts
+++ packages/client-core/src/configs.ts
@@ -15,16 +15,6 @@ export const PartialConfig = {
     logging: true,
     userSeed: "nillion-devnet",
   },
-  Nucleus: {
-    network: NamedNetwork.enum.Nucleus,
-    cluster: "3272dd62-b126-466e-92f2-69fcc2c62ab6",
-    bootnodes: [
-      "/dns/node-1.testnet-nucleus.nillion-network.nilogy.xyz/tcp/14211/wss/p2p/12D3KooWFH5doiPHBJa8cgx7B2zzD7z7DbyKzRJPmsTZFHFT5zyc",
-    ],
-    chain: "nillion-chain-testnet-1",
-    endpoint: "http://65.109.222.111:26657",
-    logging: true,
-  },
   Photon: {
     network: NamedNetwork.enum.Photon,
     cluster: "b13880d3-dde8-4a75-a171-8a1a9d985e6c",
@@ -32,7 +22,7 @@ export const PartialConfig = {
       "/dns/node-1.testnet-photon.nillion-network.nilogy.xyz/tcp/14211/wss/p2p/12D3KooWCfFYAb77NCjEk711e9BVe2E6mrasPZTtAjJAPtVAdbye",
     ],
     chain: "nillion-chain-testnet-1",
-    endpoint: "http://65.109.222.111:26657",
+    endpoint: "https://testnet-nillion-rpc.lavenderfive.com",
     logging: false,
   },
 };
