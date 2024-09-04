5e445ae87e180a15ebd4be3eed61d873e17cfe43
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-09-04 12:16:03 +0100

    docs(nextjs): drop local proxy from config
    This appears to be working as without the proxy so removing it.

--- examples/nextjs/next.config.mjs
+++ examples/nextjs/next.config.mjs
@@ -1,37 +1,15 @@
 /* eslint-disable */
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // config.resolve.fallback = {
-    //   crypto: false,
-    //   stream: false,
-    //   buffer: false,
-    //   vm: false,
-    // };
-    //
-    // config.module.rules.push({
-    //   test: /\.wasm$/,
-    //   type: "asset/resource",
-    // });
-
-    return config;
-  },
   async headers() {
     return [
       {
         source: "/:path*",
+        // headers required for @nillion/client-wasm's wasm artefact
         headers: [
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
         ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/nilchain",
-        destination: "http://127.0.0.1:48102/",
       },
     ];
   },
