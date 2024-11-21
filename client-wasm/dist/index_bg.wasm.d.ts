a426d7ea0d8b430b56b8b6b57d250bb9b19a6e76
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-11-21 08:34:02 +0000

    chore(wasm): bump artefacts to nillion `v0.7.0-rc.55` for release

--- client-wasm/dist/index_bg.wasm.d.ts
+++ client-wasm/dist/index_bg.wasm.d.ts
@@ -2,100 +2,40 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function __wbg_nadavalue_free(a: number): void;
-export function nadavalue_new_secret_integer(
-  a: number,
-  b: number,
-  c: number,
-): void;
-export function nadavalue_new_secret_unsigned_integer(
-  a: number,
-  b: number,
-  c: number,
-): void;
+export function nadavalue_new_secret_integer(a: number, b: number, c: number): void;
+export function nadavalue_new_secret_unsigned_integer(a: number, b: number, c: number): void;
 export function nadavalue_new_secret_blob(a: number, b: number): number;
-export function nadavalue_new_public_integer(
-  a: number,
-  b: number,
-  c: number,
-): void;
-export function nadavalue_new_public_unsigned_integer(
-  a: number,
-  b: number,
-  c: number,
-): void;
+export function nadavalue_new_public_integer(a: number, b: number, c: number): void;
+export function nadavalue_new_public_unsigned_integer(a: number, b: number, c: number): void;
 export function nadavalue_to_byte_array(a: number, b: number): void;
 export function nadavalue_to_integer(a: number, b: number): void;
 export function nadavalue_type_name(a: number, b: number): void;
 export function __wbg_nadavalues_free(a: number): void;
 export function nadavalues_new(a: number): void;
-export function nadavalues_insert(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): void;
+export function nadavalues_insert(a: number, b: number, c: number, d: number): void;
 export function nadavalues_length(a: number): number;
 export function nadavalues_to_record(a: number, b: number): void;
 export function __wbg_partyid_free(a: number): void;
 export function partyid_new(a: number, b: number): number;
 export function partyid_to_byte_array(a: number): number;
 export function __wbg_secretmasker_free(a: number): void;
-export function secretmasker_new_64_bit_safe_prime(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): void;
-export function secretmasker_new_128_bit_safe_prime(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): void;
-export function secretmasker_new_256_bit_safe_prime(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): void;
+export function secretmasker_new_64_bit_safe_prime(a: number, b: number, c: number, d: number): void;
+export function secretmasker_new_128_bit_safe_prime(a: number, b: number, c: number, d: number): void;
+export function secretmasker_new_256_bit_safe_prime(a: number, b: number, c: number, d: number): void;
 export function secretmasker_mask(a: number, b: number, c: number): void;
-export function secretmasker_unmask(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): void;
+export function secretmasker_unmask(a: number, b: number, c: number, d: number): void;
 export function secretmasker_classify_values(a: number, b: number): number;
 export function __wbg_nadavaluesclassification_free(a: number): void;
 export function __wbg_get_nadavaluesclassification_particles(a: number): number;
-export function __wbg_set_nadavaluesclassification_particles(
-  a: number,
-  b: number,
-): void;
+export function __wbg_set_nadavaluesclassification_particles(a: number, b: number): void;
 export function __wbg_get_nadavaluesclassification_shares(a: number): number;
-export function __wbg_set_nadavaluesclassification_shares(
-  a: number,
-  b: number,
-): void;
+export function __wbg_set_nadavaluesclassification_shares(a: number, b: number): void;
 export function __wbg_get_nadavaluesclassification_public(a: number): number;
-export function __wbg_set_nadavaluesclassification_public(
-  a: number,
-  b: number,
-): void;
-export function __wbg_get_nadavaluesclassification_ecdsa_private_key_shares(
-  a: number,
-): number;
-export function __wbg_set_nadavaluesclassification_ecdsa_private_key_shares(
-  a: number,
-  b: number,
-): void;
-export function __wbg_get_nadavaluesclassification_ecdsa_signature_shares(
-  a: number,
-): number;
-export function __wbg_set_nadavaluesclassification_ecdsa_signature_shares(
-  a: number,
-  b: number,
-): void;
+export function __wbg_set_nadavaluesclassification_public(a: number, b: number): void;
+export function __wbg_get_nadavaluesclassification_ecdsa_private_key_shares(a: number): number;
+export function __wbg_set_nadavaluesclassification_ecdsa_private_key_shares(a: number, b: number): void;
+export function __wbg_get_nadavaluesclassification_ecdsa_signature_shares(a: number): number;
+export function __wbg_set_nadavaluesclassification_ecdsa_signature_shares(a: number, b: number): void;
 export function __wbg_partyshares_free(a: number): void;
 export function partyshares_new(a: number, b: number, c: number): void;
 export function partyshares_party(a: number): number;
@@ -109,17 +49,9 @@ export function programmetadata_new(a: number, b: number, c: number): void;
 export function programmetadata_memory_size(a: number): number;
 export function programmetadata_total_instructions(a: number): number;
 export function programmetadata_instructions(a: number, b: number): void;
-export function programmetadata_preprocessing_requirements(
-  a: number,
-  b: number,
-): void;
+export function programmetadata_preprocessing_requirements(a: number, b: number): void;
 export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(
-  a: number,
-  b: number,
-  c: number,
-  d: number,
-): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
