a0aeb030c5e00303c69f3077c7d117535d216b8a
Pablo Hernández <pablo.hernandez@nillion.com>
2024-12-16 20:40:10 +0000

    feat: add support for top up payments

--- client-vms/tests/payments.test.ts
+++ client-vms/tests/payments.test.ts
@@ -1,19 +1,26 @@
 import { create, fromBinary } from "@bufbuild/protobuf";
-import { createGrpcWebTransport } from "@connectrpc/connect-web";
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { ZodError } from "zod";
 import { PriceQuoteRequestSchema } from "#/gen-proto/nillion/payments/v1/quote_pb";
 import { ReceiptSchema } from "#/gen-proto/nillion/payments/v1/receipt_pb";
-import {
-  type PaymentClient,
-  PaymentClientBuilder,
-  createSignerFromKey,
-} from "#/payment";
-import { fetchClusterDetails } from "#/vm";
+import { PaymentClientBuilder, createSignerFromKey } from "#/payment";
+import { type VmClient, VmClientBuilder } from "#/vm";
 import { Env, PrivateKeyPerSuite } from "./helpers";
 
 describe("PaymentClient", () => {
-  let client: PaymentClient;
+  let client: VmClient;
+
+  beforeAll(async () => {
+    const signer = await createSignerFromKey(PrivateKeyPerSuite.VmClient);
+
+    client = await new VmClientBuilder()
+      // Random seed is required to avoid failures when the tests are run multiple times consecutively
+      .seed(Math.random().toString(36))
+      .bootnodeUrl(Env.bootnodeUrl)
+      .chainUrl(Env.nilChainUrl)
+      .signer(signer)
+      .build();
+  });
 
   it("builder rejects if missing values", async () => {
     try {
@@ -21,29 +28,10 @@ describe("PaymentClient", () => {
       await builder.build();
     } catch (e) {
       expect(e).toBeInstanceOf(ZodError);
-      expect((e as ZodError).issues).toHaveLength(3);
+      expect((e as ZodError).issues).toHaveLength(5);
     }
 
     expect.assertions(2);
-  });
-
-  it("builder can create client", async () => {
-    const cluster = await fetchClusterDetails(Env.bootnodeUrl);
-    const signer = await createSignerFromKey(PrivateKeyPerSuite.Payments);
-    const builder = new PaymentClientBuilder();
-
-    const leader = createGrpcWebTransport({
-      baseUrl: cluster.leader?.grpcEndpoint ?? "",
-      useBinaryFormat: true,
-    });
-
-    client = await builder
-      .chainUrl(Env.nilChainUrl)
-      .leader(leader)
-      .signer(signer)
-      .build();
-
-    expect(client).toBeDefined();
   });
 
   it("can pay for an operation", async () => {
@@ -53,9 +41,47 @@ describe("PaymentClient", () => {
         value: {},
       },
     });
-    const signedReceipt = await client.payForOperation(request);
+    const signedReceipt = await client.payer.payForOperation(request);
     const receipt = fromBinary(ReceiptSchema, signedReceipt.receipt);
     expect(receipt.expiresAt?.seconds).toBeGreaterThan(new Date().getSeconds());
     expect(receipt.identifier).toHaveLength(16);
+  });
+
+  it("can query payments config", async () => {
+    const paymentsConfig = await client.payer.paymentsConfig();
+    expect(paymentsConfig.minimumAddFundsPayment).toBeDefined;
+  });
+
+  it("can query account balance", async () => {
+    const account = await client.payer.accountBalance();
+    expect(account.balance).toBe(BigInt(0));
+  });
+
+  it("add not enough funds", async () => {
+    await expect(() => client.payer.addFunds(BigInt(0))).rejects.toThrowError(
+      "Not enough unil amount",
+    );
+  });
+
+  it("add enough funds", async () => {
+    const result = await client.payer.addFunds(BigInt(10));
+    expect(result).toBeNull;
+  });
+
+  it("account is funded", async () => {
+    const account = await client.payer.accountBalance();
+    expect(account.balance).toBe(BigInt(10));
+  });
+
+  it("can pay from balance", async () => {
+    const request = create(PriceQuoteRequestSchema, {
+      operation: {
+        case: "poolStatus",
+        value: {},
+      },
+    });
+    await client.payer.payForOperation(request);
+    const account = await client.payer.accountBalance();
+    expect(account.balance).toBeLessThan(BigInt(10));
   });
 });
